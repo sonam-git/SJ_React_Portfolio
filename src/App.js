16c627c484970d11f4d2b95427d9ad479e076d30
Sonam Sherpa <sherpa.sjs@gmail.com>
2023-06-29 22:03:11 -0700

    updated hompage url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
-import Contact from './components/Contact';
-import Resume from './components/Resume';
-import Footer from './components/Footer';
-import Header from './components/Header'; 
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import "./App.css";
-
 
 const linksArray = [
   { to: "/about", label: "About Me" },
@@ -19,14 +18,14 @@ function App() {
   return (
     <div className="App">
       <Router>
-      <Header links={linksArray} />
+        <Header links={linksArray} />
         <div className="container mt-5">
           <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
+            <Route path="/SJ_React_Portfolio/" element={<About />} />
+            <Route path="/SJ_React_Portfolio/about" element={<About />} />
+            <Route path="/SJ_React_Portfolio/portfolio" element={<Portfolio />}/>
+            <Route path="/SJ_React_Portfolio/contact" element={<Contact />} />
+            <Route path="/SJ_React_Portfolio/resume" element={<Resume />} />
           </Routes>
         </div>
         <Footer />
