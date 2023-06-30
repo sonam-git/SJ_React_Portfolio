2bd345b91ef2961339d41390abfb3bf5796f0c3e
Sonam Sherpa <sherpa.sjs@gmail.com>
2023-06-29 18:52:29 -0700

    url updated for each component

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import "./App.css";
 
 
 const linksArray = [
-  { to: "/about", label: "About Me" },
-  { to: "/portfolio", label: "Portfolio" },
-  { to: "/contact", label: "Contact" },
-  { to: "/resume", label: "Resume" },
+  { to: "/SJ_React_Portfolio/about", label: "About Me" },
+  { to: "/SJ_React_Portfolio/portfolio", label: "Portfolio" },
+  { to: "/SJ_React_Portfolio/contact", label: "Contact" },
+  { to: "/SJ_React_Portfolio/resume", label: "Resume" },
 ];
 
 function App() {
@@ -22,11 +22,11 @@ function App() {
       <Header links={linksArray} />
         <div className="container mt-5">
           <Routes>
-            <Route path="/" element={<About />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
+            <Route path="/SJ_React_Portfolio/" element={<About />} />
+            <Route path="/SJ_React_Portfolio/about" element={<About />} />
+            <Route path="/SJ_React_Portfolio/portfolio" element={<Portfolio />} />
+            <Route path="/SJ_React_Portfolio/contact" element={<Contact />} />
+            <Route path="/SJ_React_Portfolio/resume" element={<Resume />} />
           </Routes>
         </div>
         <Footer />
